575f4868d9735044f197b5a92e19aced2f6d670b
Ifanoxy <alwindoudou@gmail.com>
2024-06-19 20:15:51 +0200

    feat: GSAP + landing page

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <div>
     <nav class="bg-dark">
       <div class="max-w-screen flex flex-wrap items-center justify-between mx-auto p-4 ml-0">
-        <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+        <a href="/" class="w-80 flex items-center space-x-3 rtl:space-x-reverse">
           <img src="@/assets/logo.svg" class="h-12" alt="Flowbite Logo" />
           <span
             class="logo-text self-center text-2xl font-black whitespace-nowrap text-white duration-500"
@@ -12,7 +12,7 @@
             ><span>A</span><span>K</span><span>E</span><span>R</span>
           </span>
         </a>
-        <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+        <div class="w-80 justify-end flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <button
             type="button"
             class="text-dark font-black bg-primary duration-300 hover:bg-white hover:rounded-xl rounded-lg text-lg px-20 py-2 text-center"
@@ -21,30 +21,29 @@
           </button>
         </div>
         <div
-          class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1 xl:mr-40"
+          class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
           id="navbar-cta"
         >
           <ul
-            class="flex flex-col font-medium p-4 md:p-0 mt-4 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0"
+            class="flex gap-4 flex-col font-medium p-4 md:p-0 mt-4 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0"
           >
             <li>
-              <a
-                href="#"
-                class="block nav-items py-2 px-3 md:p-0 font-normal text-lg text-white rounded"
+              <a href="#" class="block nav-items py-2 md:p-0 font-normal text-lg text-white rounded"
                 >GITHUB</a
               >
             </li>
             <li>
-              <a
-                href="#"
-                class="block nav-items py-2 px-3 md:p-0 font-normal text-lg text-white rounded"
-                >MODULES</a
+              <a href="#" class="block nav-items py-2 md:p-0 font-normal text-lg text-white rounded"
+                >SUPPORT</a
               >
             </li>
             <li>
-              <a
-                href="#"
-                class="block nav-items py-2 px-3 md:p-0 font-normal text-lg text-white rounded"
+              <a href="#" class="block nav-items py-2 md:p-0 font-normal text-lg text-white rounded"
+                >DASHBOARD</a
+              >
+            </li>
+            <li>
+              <a href="#" class="block nav-items py-2 md:p-0 font-normal text-lg text-white rounded"
                 >ABOUT</a
               >
             </li>
@@ -107,7 +106,8 @@
   border-radius: 5px;
   border-width: 2px;
   border-color: transparent;
-  padding: 0 10px;
+  padding: 4px 10px 0;
+
   &:hover {
     border-color: var(--primary);
     color: var(--primary);
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,244 @@
-<template>home</template>
+<template>
+  <navbar />
+  <div class="cursor" ref="cursor"></div>
+  <div class="cursor-follower" ref="follower"></div>
+  <div class="flex flex-col w-full h-auto">
+    <div
+      class="hero mt-40 sm:flex-col xl:flex-row flex text-white h-4/5 items-center w-full justify-center font-black uppercase"
+    >
+      <div class="box h-60 select-none flex text-8xl w-full justify-center items-center" ref="box">
+        <Transition name="fade">
+          <h3
+            v-on:mouseover="startAnimation"
+            v-show="animationEnd"
+            class="select-none 2xl:text-7xl text-5xl"
+          >
+            Discord Bot Maker
+          </h3>
+        </Transition>
+      </div>
+      <div class="flex justify-center w-full">
+        <img class="inset-0 object-contain max-w-2xl" src="@/assets/logo.png" />
+      </div>
+    </div>
+  </div>
+</template>
 
-<style scoped></style>
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import Navbar from '@/components/Navbar.vue'
+import gsap from 'gsap'
+
+export default defineComponent({
+  components: { Navbar },
+  data() {
+    return {
+      text: '',
+      animationEnd: true
+    }
+  },
+  mounted() {
+    this.startAnimation()
+    this.cursor()
+  },
+  methods: {
+    cursor() {
+      const { cursor, follower } = this.$refs
+
+      let posX = 0
+      let posY = 0
+      let mouseX = 0
+      let mouseY = 0
+
+      const TM = gsap.timeline()
+
+      TM.to({}, 0.005, {
+        repeat: -1,
+        onRepeat: function () {
+          posX += (mouseX - posX) / 9
+          posY += (mouseY - posY) / 9
+
+          TM.set(follower, {
+            css: {
+              left: posX - 12,
+              top: posY - 12
+            }
+          })
+
+          TM.set(cursor, {
+            css: {
+              left: mouseX,
+              top: mouseY
+            }
+          })
+        }
+      })
+
+      const animateCursor = () => {
+        posX += (mouseX - posX) / 9
+        posY += (mouseY - posY) / 9
+
+        if (follower.value) {
+          gsap.set(follower.value, {
+            css: {
+              left: posX - 12,
+              top: posY - 12
+            }
+          })
+        }
+
+        if (cursor.value) {
+          gsap.set(cursor.value, {
+            css: {
+              left: mouseX,
+              top: mouseY
+            }
+          })
+        }
+      }
+
+      requestAnimationFrame(animateCursor)
+
+      document.addEventListener('mousemove', (e) => {
+        mouseX = e.clientX
+        mouseY = e.clientY
+      })
+
+      document.querySelectorAll('.link').forEach((element) => {
+        element.addEventListener('mouseenter', () => {
+          cursor.value?.classList.add('active')
+          follower.value?.classList.add('active')
+        })
+        element.addEventListener('mouseleave', () => {
+          cursor.value?.classList.remove('active')
+          follower.value?.classList.remove('active')
+        })
+      })
+    },
+    startAnimation() {
+      if (!this.animationEnd) return
+      this.animationEnd = false
+      const { box } = this.$refs
+      const words = ['welcome', 'at', 'the', 'best', 'discord', 'bot', 'maker.']
+      const timeline = gsap.timeline({
+        delay: 0.6,
+        onComplete: () => (this.animationEnd = true)
+      })
+
+      let wordCount = words.length,
+        time = 0,
+        word,
+        element,
+        duration,
+        isSentenceEnd,
+        i
+
+      for (i = 0; i < wordCount; i++) {
+        word = words[i]
+        isSentenceEnd = /(\.|\?|!)$/g.test(word)
+        element = document.createElement('h3')
+        element.textContent = word
+        box.append(element)
+        duration = Math.max(0.55, word.length * 0.08)
+        if (isSentenceEnd) {
+          duration += 0.6
+        }
+
+        gsap.set(element, { autoAlpha: 0, scale: 0, z: 0.01 })
+
+        timeline
+          .to(element, duration, { scale: 1.2, ease: 'slow(0.20, 0.9)' }, time)
+          .to(element, duration, { autoAlpha: 1, ease: 'slow(0.20, 0.9, true)' }, time)
+          .to(element, duration, { autoAlpha: 0, ease: 'slow(0.1, 0.5)' }, time + duration - 0.25)
+
+        time += duration - 0.05
+        if (isSentenceEnd) {
+          time += 0.6
+        }
+      }
+    }
+  }
+})
+</script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.box {
+  h3 {
+    position: absolute;
+  }
+}
+
+body {
+  cursor: none;
+}
+
+.cursor {
+  position: fixed;
+  background-color: var(--primary);
+  width: 10px;
+  height: 10px;
+  border-radius: 100%;
+  transition:
+    0.3s cubic-bezier(0.75, -1.27, 0.3, 2.33) transform,
+    0.2s cubic-bezier(0.75, -0.27, 0.3, 1.33) opacity;
+  user-select: none;
+  pointer-events: none;
+  z-index: 10000;
+  transform: scale(1);
+
+  &.active {
+    opacity: 1;
+    transform: scale(0);
+  }
+
+  &.menu-active {
+    opacity: 1;
+    transform: scale(0);
+  }
+
+  &.hovered {
+    opacity: 1;
+  }
+}
+
+.cursor-follower {
+  position: fixed;
+  border: 1px solid black;
+  width: 30px;
+  height: 30px;
+  border-radius: 100%;
+  z-index: 1;
+  transition:
+    0.6s cubic-bezier(0.75, -1.27, 0.3, 2.33) transform,
+    0.2s cubic-bezier(0.75, -0.27, 0.3, 1.33) opacity,
+    0.2s cubic-bezier(0.75, -0.27, 0.3, 1.33) background;
+  user-select: none;
+  pointer-events: none;
+  z-index: 10000;
+  transform: translate(2px, 2px);
+
+  &.active {
+    opacity: 1;
+    transform: scale(3);
+  }
+
+  &.menu-active {
+    opacity: 1;
+    transform: scale(3);
+  }
+
+  &.hovered {
+    opacity: 1;
+  }
+}
+</style>
