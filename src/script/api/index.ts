378c092fe225cc84783ac6326281950580d45818
Ifanoxy <alwindoudou@gmail.com>
2024-07-19 11:15:26 +0200

    fix: fix login

--- src/script/api/index.ts
+++ src/script/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:8080/'
+const BASE_URL = 'https://api.discordbotmaker.fr/'
 
 const getAccess = () => localStorage.getItem('access_token')
 
